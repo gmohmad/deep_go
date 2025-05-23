3429d89855b0e04f24d2dbc98a485b0596d06c71
gmohmad <germihanovmohmad@gmail.com>
2025-05-23 20:44:28 +0300

    HW9 done

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"slices"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
+
+const (
+	PropertiesTag   = "properties"
+	OmitemptyTagVal = "omitempty"
+)
 
 type Person struct {
 	Name    string `properties:"name"`
@@ -15,9 +24,23 @@ type Person struct {
 	Married bool   `properties:"married"`
 }
 
-func Serialize(person Person) string {
-	// need to implement
-	return ""
+func Serialize[T any](object T) string {
+	builder := strings.Builder{}
+	objectType := reflect.TypeOf(object)
+
+	for i := range objectType.NumField() {
+		fieldValue := reflect.ValueOf(object).Field(i)
+		tagValues := strings.Split(objectType.Field(i).Tag.Get(PropertiesTag), ",")
+
+		omitempty := slices.Contains(tagValues, OmitemptyTagVal)
+		if len(tagValues) == 0 || fieldValue.IsZero() && omitempty {
+			continue
+		}
+
+		builder.WriteString(fmt.Sprintf("%s=%v\n", tagValues[0], fieldValue))
+	}
+
+	return strings.TrimRight(builder.String(), "\n")
 }
 
 func TestSerialization(t *testing.T) {
